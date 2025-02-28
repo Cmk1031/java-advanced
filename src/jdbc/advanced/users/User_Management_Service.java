69bb0876571af5cab2a0ab38d9972846c1a1d1d9
Cmk1031 <maple1031@naver.com>
2025-02-28 14:51:19 +0900

    storedProcedure

--- src/jdbc/advanced/users/User_Management_Service.java
+++ src/jdbc/advanced/users/User_Management_Service.java
@@ -8,35 +8,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+
 /**
  * packageName   : jdbc.advanced.users
  * fileName      : User_Management_Service
  * author        : a
  * date          : 2025-02-28
  * description   :
- *
- * 	회원을 관리하는 프로그램을 작성하시오.   => 자신 코드 스타일로 stream, lamda 사용하셔도 됩니다.
- * 	(어제 만든 ssgdb.users 테이블 이용)
- *
- * 	아래 메뉴의 기능을 모두 구현하시오. (CRUD기능 구현하기)
- * 	메뉴 예시)
- * 		== 작업 선택 ==
- * 		1. 자료 추가
- * 		2. 자료 삭제
- * 		3. 자료 수정
- * 		4. 전체 자료 출력
- * 		0. 작업 끝.
- *
- * 	처리조건)
- * 	1) 자료 추가에서 '회원ID'는 중복되지 않는다.(중복되면 다시 입력 받는다.)
- * 	2) 삭제는 '회원ID'를 입력 받아서 처리한다.
- * 	3) 자료 수정에서 '회원ID'는 변경되지 않는다.
- *
- *
- * */
+ * <p>
+ * 회원을 관리하는 프로그램을 작성하시오.   => 자신 코드 스타일로 stream, lamda 사용하셔도 됩니다.
+ * (어제 만든 ssgdb.users 테이블 이용)
+ * <p>
+ * 아래 메뉴의 기능을 모두 구현하시오. (CRUD기능 구현하기)
+ * 메뉴 예시)
+ * == 작업 선택 ==
+ * 1. 자료 추가
+ * 2. 자료 삭제
+ * 3. 자료 수정
+ * 4. 전체 자료 출력
+ * 0. 작업 끝.
+ * <p>
+ * 처리조건)
+ * 1) 자료 추가에서 '회원ID'는 중복되지 않는다.(중복되면 다시 입력 받는다.)
+ * 2) 삭제는 '회원ID'를 입력 받아서 처리한다.
+ * 3) 자료 수정에서 '회원ID'는 변경되지 않는다.
+ */
 
 public class User_Management_Service {
-
 
 
     private Scanner scan = new Scanner(System.in);
@@ -47,32 +45,36 @@ public class User_Management_Service {
     }
 
 
-
-    public void memberStart(){
-        while(true){
+    public void memberStart() {
+        while (true) {
             int choice = displayMenu();
-            switch(choice){
-                case 1 :	// 추가
-                    insertMember(); break;
-                case 2 : 	// 삭제
-                    deleteMember(); break;
-                case 3 : 	// 수정  ==> 전체 항목 수정
-                    updateMember(); break;
-                case 4 : 	// 전체 자료 출력
-                    displayMember(); break;
-                case 5 : 	// 수정	==> 원하는 항목만 수정
-                    updateMember2(); break;
-                case 0 : 	// 종료
+            switch (choice) {
+                case 1:    // 추가
+                    insertMember();
+                    break;
+                case 2:    // 삭제
+                    deleteMember();
+                    break;
+                case 3:    // 수정  ==> 전체 항목 수정
+                    updateMember();
+                    break;
+                case 4:    // 전체 자료 출력
+                    displayMember();
+                    break;
+                case 5:    // 수정	==> 원하는 항목만 수정
+                    updateMember2();
+                    break;
+                case 0:    // 종료
                     System.out.println("작업을 마칩니다.");
                     return;
-                default :
+                default:
                     System.out.println("번호를 잘못 입력했습니다. 다시입력하세요.");
             }
         }
     }
 
     // 회원 정보를 추가하는 메서드
-    private void insertMember(){
+    private void insertMember() {
         Connection conn = null;
         PreparedStatement pstmt = null;
 
@@ -81,16 +83,16 @@ public class User_Management_Service {
 
         int count = 0;
         String memId = null;  // 회원ID가 저장될 변수
-        do{
+        do {
             System.out.print("회원ID >> ");
             memId = scan.next();
             count = getMemberCount(memId);
-            if(count>0){
+            if (count > 0) {
                 System.out.println(memId + "은(는) 이미 등록된 회원ID입니다.");
                 System.out.println("다른 회원ID를 입력하세요.");
             }
 
-        }while(count>0);
+        } while (count > 0);
 
         System.out.print("회원이름 >> ");
         String memName = scan.next();
@@ -122,9 +124,9 @@ public class User_Management_Service {
 
             int cnt = pstmt.executeUpdate();
 
-            if( cnt>0 ){
+            if (cnt > 0) {
                 System.out.println("회원 정보 추가 성공!!!");
-            }else{
+            } else {
                 System.out.println("회원 정보 추가 실패~~~");
             }
 
@@ -132,14 +134,20 @@ public class User_Management_Service {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if(pstmt!=null) try{ pstmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
 
     }
 
     // 회원 정보를 삭제하는 메서드
-    private void deleteMember(){
+    private void deleteMember() {
         Connection conn = null;
         PreparedStatement pstmt = null;
 
@@ -160,9 +168,9 @@ public class User_Management_Service {
 
             int cnt = pstmt.executeUpdate();
 
-            if(cnt>0){
+            if (cnt > 0) {
                 System.out.println("회원ID가 " + memId + "인 회원 삭제 성공!!");
-            }else{
+            } else {
                 System.out.println(memId + "은(는) 없는 회원ID이거나 "
                         + "삭제에 실패했습니다.");
             }
@@ -170,13 +178,19 @@ public class User_Management_Service {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if(pstmt!=null) try{ pstmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
     }
 
     // 회원 정보를 수정하는 메서드  ==> 전체 항목 수정
-    private void updateMember(){
+    private void updateMember() {
         Connection conn = null;
         PreparedStatement pstmt = null;
 
@@ -186,7 +200,7 @@ public class User_Management_Service {
         String memId = scan.next();
 
         int count = getMemberCount(memId);
-        if(count==0){ // 없는 회원이면...
+        if (count == 0) { // 없는 회원이면...
             System.out.println(memId + "은(는) 없는 회원ID입니다.");
             System.out.println("수정 작업을 중단합니다.");
             return;
@@ -210,41 +224,47 @@ public class User_Management_Service {
         try {
             conn = DBUtil.getConnection();
 
-             String sql = new StringBuilder()
-                     .append("UPDATE USERS SET ")
-                     .append("memName = ?, ")
-                     .append("memPass = ?, ")
-                     .append("memTel = ?, ")
-                     .append("memAddr = ? ")
-                     .append("WHERE memId = ?").toString();
+            String sql = new StringBuilder()
+                    .append("UPDATE USERS SET ")
+                    .append("memName = ?, ")
+                    .append("memPass = ?, ")
+                    .append("memTel = ?, ")
+                    .append("memAddr = ? ")
+                    .append("WHERE memId = ?").toString();
 
-             pstmt = conn.prepareStatement(sql);
-             pstmt.setString(1, newMemName);
-             pstmt.setString(2, newMemPass);
-             pstmt.setString(3, newMemTel);
-             pstmt.setString(4, newMemAddr);
-             pstmt.setString(5, memId);
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, newMemName);
+            pstmt.setString(2, newMemPass);
+            pstmt.setString(3, newMemTel);
+            pstmt.setString(4, newMemAddr);
+            pstmt.setString(5, memId);
 
-             int cnt = pstmt.executeUpdate();
+            int cnt = pstmt.executeUpdate();
 
-            if(cnt>0){
+            if (cnt > 0) {
                 System.out.println(memId + "회원 정보 수정 완료!!!");
-            }else{
+            } else {
                 System.out.println(memId + "회원 정보 수정 실패~~~");
             }
 
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally{
-            if(pstmt!=null) try{ pstmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+        } finally {
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
 
     }
 
     // 회원 정보를 수정하는 메서드  ==> 원하는 항목 수정
-    private void updateMember2(){
+    private void updateMember2() {
         Connection conn = null;
         PreparedStatement pstmt = null;
 
@@ -254,7 +274,7 @@ public class User_Management_Service {
         String memId = scan.next();
 
         int count = getMemberCount(memId);
-        if(count==0){ // 없는 회원이면...
+        if (count == 0) { // 없는 회원이면...
             System.out.println(memId + "은(는) 없는 회원ID입니다.");
             System.out.println("수정 작업을 중단합니다.");
             return;
@@ -263,7 +283,7 @@ public class User_Management_Service {
         int num; // 수정할 컬럼에 대한 선택 값이 저장될 변수
         String updateField = null;
         String updateTitle = null;
-        do{
+        do {
             System.out.println();
             System.out.println("수정할 항목을 선택하세요.");
             System.out.println(" 1.회원이름  2.비밀번호  3.전화번호  4.회원주소");
@@ -271,20 +291,28 @@ public class User_Management_Service {
             System.out.print("수정할 항목 선택 >> ");
             num = scan.nextInt();
 
-            switch(num){
-                case 1 : updateField = "memName";
-                    updateTitle = "회원이름"; break;
-                case 2 : updateField = "memPass";
-                    updateTitle = "비밀번호"; break;
-                case 3 : updateField = "memTel";
-                    updateTitle = "전화번호"; break;
-                case 4 : updateField = "memAddr";
-                    updateTitle = "회원주소"; break;
-                default :
+            switch (num) {
+                case 1:
+                    updateField = "memName";
+                    updateTitle = "회원이름";
+                    break;
+                case 2:
+                    updateField = "memPass";
+                    updateTitle = "비밀번호";
+                    break;
+                case 3:
+                    updateField = "memTel";
+                    updateTitle = "전화번호";
+                    break;
+                case 4:
+                    updateField = "memAddr";
+                    updateTitle = "회원주소";
+                    break;
+                default:
                     System.out.println("수정할 항목을 잘못 선택했습니다.");
                     System.out.println("다시 선택하세요.");
             }
-        }while(num<1 || num>4);
+        } while (num < 1 || num > 4);
 
         scan.nextLine();  // 입력 버퍼 비우기
         System.out.println();
@@ -294,7 +322,7 @@ public class User_Management_Service {
         try {
             conn = DBUtil.getConnection();
 
-            /*String sql = new StringBuilder()
+            String sql = new StringBuilder()
                     .append("UPDATE USERS SET ")
                     .append(updateField)
                     .append(" = ?")
@@ -302,39 +330,35 @@ public class User_Management_Service {
 
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, updateData);
-            pstmt.setString(2, memId);*/
-
-            String sql = new StringBuilder()
-                    .append("UPDATE USERS SET ")
-                    .append("? = ? ")
-                    .append("WHERE memId = ?").toString();
-
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, updateField);
-            pstmt.setString(2, updateData);
-            pstmt.setString(3, memId);
+            pstmt.setString(2, memId);
 
 
             int cnt = pstmt.executeUpdate();
 
-            if(cnt>0){
+            if (cnt > 0) {
                 System.out.println(memId + " 회원 정보 수정 완료!!!");
-            }else{
+            } else {
                 System.out.println(memId + " 회원 정보 수정 실패~~~");
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if(pstmt!=null) try{ pstmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
 
     }
 
 
     // 전체 회원 정보를 출력하는 메서드
-    private void displayMember(){
+    private void displayMember() {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
@@ -371,15 +395,24 @@ public class User_Management_Service {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if(rs!=null) try{ rs.close();   }catch(SQLException e){}
-            if(stmt!=null) try{ stmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+            if (rs != null) try {
+                rs.close();
+            } catch (SQLException e) {
+            }
+            if (stmt != null) try {
+                stmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
 
     }
 
     // 회원ID를 인수값으로 받아서 해당 회원ID의 개수를 반환하는 메서드
-    private int getMemberCount(String memId){
+    private int getMemberCount(String memId) {
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
@@ -394,7 +427,7 @@ public class User_Management_Service {
 
             rs = pstmt.executeQuery(sql);
             while (rs.next()) {
-                if(rs.getString("memId").equals(memId)) count++;
+                if (rs.getString("memId").equals(memId)) count++;
             }
 
 
@@ -402,9 +435,18 @@ public class User_Management_Service {
             count = 0;
             e.printStackTrace();
         } finally {
-            if(rs!=null) try{ rs.close();   }catch(SQLException e){}
-            if(pstmt!=null) try{ pstmt.close();   }catch(SQLException e){}
-            if(conn!=null) try{ conn.close();   }catch(SQLException e){}
+            if (rs != null) try {
+                rs.close();
+            } catch (SQLException e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (SQLException e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (SQLException e) {
+            }
         }
 
         return count;
@@ -412,7 +454,7 @@ public class User_Management_Service {
     }
 
     // 메뉴를 출력하고 선택한 작업 번호를 반환하는 메서드
-    private int displayMenu(){
+    private int displayMenu() {
         System.out.println();
         System.out.println("== 작업 선택 ==");
         System.out.println("1. 자료 추가 ");
